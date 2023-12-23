ea911ebcb969bbfd93a11d8a084e1592475786d6
Geripanca <geri@upi.edu>
2023-12-23 23:36:50 +0700

    nambahin aj

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import datetime  
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-	
 def index():
-    year = datetime.datetime.now().year  
+    year = datetime.datetime.now().year
     return render_template("index.html", year=year)
 
+@app.route("/tentang")
+def tentang():
+    return render_template('tentang.html')
+@app.route("/proyek")
+def proyek():
+    return render_template('proyek.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)		
