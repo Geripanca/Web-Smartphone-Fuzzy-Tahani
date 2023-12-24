7fb6cf46ffc90b52b54935a4d080a0a96e8cbdd0
Geripanca <geri@upi.edu>
2023-12-24 18:29:39 +0700

    sedikit perubahan file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
---
+++ config.py
@@ -0,0 +1,8 @@
+# Konfigurasi database
+SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/mydb'
+
+# Konfigurasi keamanan
+SECRET_KEY = 'rahasia'
+
+# Konfigurasi lainnya
+DEBUG = True
